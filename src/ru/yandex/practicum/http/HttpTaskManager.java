03a8b96e3826974c066b5ee43811f31f8c490cd6
Magicoff <danilka.3012@yandex.ru>
2024-02-12 02:16:03 +0300

    нужна помощь, а то я уже все

---
+++ src/ru/yandex/practicum/http/HttpTaskManager.java
@@ -0,0 +1,85 @@
+package ru.yandex.practicum.http;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import ru.yandex.practicum.manager.FileBackedTasksManager;
+import ru.yandex.practicum.manager.Managers;
+import ru.yandex.practicum.tasks.Epic;
+import ru.yandex.practicum.tasks.Subtask;
+import ru.yandex.practicum.tasks.Task;
+import ru.yandex.practicum.tasks.TaskType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HttpTaskManager extends FileBackedTasksManager {
+    private final Gson gson;
+    private final KVTaskClient client;
+
+    public HttpTaskManager(int port){
+        this(port,false);
+    }
+
+    public HttpTaskManager(int port,boolean load){
+        super(null);
+        gson = Managers.getGson();
+        client = new KVTaskClient(port);
+        if (load){
+            load();
+        }
+    }
+
+    protected void addTasks(List<? extends Task> tasks){
+        for (Task task : tasks){
+            final int id = task.getId();
+            if (id > generatedIds){
+                generatedIds = id;
+            }
+            TaskType type = task.getType();
+            if (type == TaskType.TASK){
+                this.tasks.put(id,task);
+                prioritizedTasks.add(task);
+            }else if (type == TaskType.SUBTASK){
+                subtasks.put(id, (Subtask) task);
+                prioritizedTasks.add(task);
+            }else if (type == TaskType.EPIC){
+                epics.put(id,(Epic) task);
+            }
+        }
+    }
+    private void load(){
+        ArrayList<Task> tasks = gson.fromJson(client.load("tasks"),new TypeToken<ArrayList<Task>>() {
+        }.getType());
+        addTasks(tasks);
+
+        ArrayList<Epic> epics = gson.fromJson(client.load("epics"),new TypeToken<ArrayList<Epic>>() {
+        }.getType());
+        addTasks(epics);
+
+        ArrayList<Subtask> subtasks = gson.fromJson(client.load("subtasks"),new TypeToken<ArrayList<Subtask>>() {
+        }.getType());
+        addTasks(subtasks);
+
+        List<Integer> history = gson.fromJson(client.load("history"),new TypeToken<ArrayList<Integer>>() {
+        }.getType());
+
+        for (Integer taskId : history){
+            historyManager.add(getTaskByIdentify(taskId));
+        }
+    }
+    @Override
+    public void save(){
+        String jsonTasks = gson.toJson(new ArrayList<>(tasks.values()));
+        client.put("tasks",jsonTasks);
+
+        String jsonSubtasks = gson.toJson(new ArrayList<>(subtasks.values()));
+        client.put("subtasks",jsonSubtasks);
+
+        String jsonEpics = gson.toJson(new ArrayList<>(epics.values()));
+        client.put("epics",jsonEpics);
+
+        String jsonHistory = gson.toJson(historyManager.getHistory().stream().map(Task::getId).collect(Collectors.toList()));
+        client.put("history",jsonHistory);
+    }
+}
--- src/ru/yandex/practicum/manager/Managers.java
+++ src/ru/yandex/practicum/manager/Managers.java
@@ -1,14 +1,30 @@
 package ru.yandex.practicum.manager;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.yandex.practicum.http.HttpTaskManager;
+import ru.yandex.practicum.server.KVServer;
+
+import java.io.IOException;
+
 public class Managers {
 
-    private Managers(){
-
-    }
+    private Managers(){}
     public static TaskManager getDefault(){
-        return new InMemoryTaskManager();
+        return new HttpTaskManager(KVServer.PORT);
     }
     public static HistoryManager getDefaultHistory(){
         return new InMemoryHistoryManager();
+    }
+
+    public static Gson getGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        return gsonBuilder.create();
+    }
+
+    public static KVServer getDefaultKVServer() throws IOException{
+        final KVServer kvserver = new KVServer();
+        kvserver.start();
+        return kvserver;
     }
 }
